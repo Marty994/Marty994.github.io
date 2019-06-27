5a788968e108c8cc7729fecd403a26c3da8a0fc1
Vlad <martymqfly@gmail.com>
2019-06-27 14:49:05 +0300

    message

--- practics/diol/js/main.js
+++ practics/diol/js/main.js
@@ -19,3 +19,27 @@ $(function() {
     }
   })();
 });
+
+function GreatBalancer(block){
+    var wrapWidth = $(block).parent().width(),  // 1
+     blockWidth = $(block).width(),          // 2
+     wrapDivide = Math.floor(wrapWidth / blockWidth),     // 3
+     cellArr = $(block);
+    for(var arg = 1;arg<=arguments.length;arg++) {           // 4.1
+     for (var i = 0; i <= cellArr.length; i = i + wrapDivide) {
+     var maxHeight = 0,
+      heightArr = [];
+     for (j = 0; j < wrapDivide; j++) {               // 4.2
+     heightArr.push($(cellArr[i + j]).find(arguments[arg]));
+      if (heightArr[j].outerHeight() > maxHeight) {
+       maxHeight = heightArr[j].outerHeight();
+      }
+     }
+     for (var counter = 0; counter < heightArr.length; counter++) {           // 4.3
+      $(cellArr[i + counter]).find(arguments[arg]).outerHeight(maxHeight);
+      }
+     }
+    }
+  }
+
+GreatBalancer(".product",".product-img",".product-title",".product-tipe",".product-price",".product-count");
